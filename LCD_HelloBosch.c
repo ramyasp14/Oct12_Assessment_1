475d05a4e8ac22f3bccc061f91a8d7db9a3fa567
ramysp14 <1rx19mca05.ramyasp@gmail.com>
2022-10-20 17:03:21 +0530

    17th CW commit

---
+++ LCD_HelloBosch.c
@@ -0,0 +1,171 @@
+/* ###################################################################
+**     Filename    : ProcessorExpert.c
+**     Project     : ProcessorExpert
+**     Processor   : MKL25Z128VLK4
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2022-10-20, 09:40, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file ProcessorExpert.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup ProcessorExpert_module ProcessorExpert module documentation
+**  @{
+*/         
+/* MODULE ProcessorExpert */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "Bits1.h"
+#include "BitsIoLdd1.h"
+#include "Bit1.h"
+#include "BitIoLdd1.h"
+#include "Bit2.h"
+#include "BitIoLdd2.h"
+#include "Bit3.h"
+#include "BitIoLdd3.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+void LCD_init()
+{
+	LCD_Command(0x01);
+	LCD_Command(0x38);
+	LCD_Command(0x0F);
+	LCD_Command(0x80);
+	LCD_Command(0x06);
+	LCD_delay();
+}
+
+void LCD_Command(unsigned char cmd)
+{
+	Bits1_PutVal(cmd);
+	Bit1_PutVal(0);
+	Bit2_PutVal(0);
+	Bit3_PutVal(1);
+	LCD_delay();
+	Bit3_PutVal(0);
+	LCD_delay();
+}
+
+void LCD_Data_1(unsigned char c)
+{
+	Bits1_PutVal(c);
+	Bit1_PutVal(1);
+	Bit2_PutVal(0);
+	Bit3_PutVal(1);
+	LCD_delay();
+	Bit3_PutVal(0);
+	LCD_delay();
+}
+
+void LCD_Data_2(unsigned char c)
+{
+	Bits1_PutVal(c);
+	Bit1_PutVal(1);
+	Bit2_PutVal(0);
+	Bit3_PutVal(1);
+	LCD_delay();
+	Bit3_PutVal(0);
+	LCD_delay();
+	
+}
+void LCD_delay()
+{
+	long int i;
+	for(i=0;i<0x7ffff;i++);
+}
+
+void LCD_display1_line()
+{
+	LCD_Data_1('H');
+	LCD_Data_1('e');
+	LCD_Data_1('l');
+	LCD_Data_1('l');
+	LCD_Data_1('o');
+	LCD_delay();
+}
+
+	
+void LCD_display2_line()
+{
+	
+	LCD_Data_2('B');
+	LCD_Data_2('o');
+	LCD_Data_2('s');
+	LCD_Data_2('c');
+	LCD_Data_2('h');
+	while(1);
+	LCD_delay();
+}
+
+
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  LCD_init();
+ 
+  while(1)
+  {
+
+	  LCD_display1_line();
+	  LCD_Command(0xC0);
+	  
+	  LCD_display2_line();
+
+	  LCD_delay();
+	  //while(1);
+			  
+  }
+  
+  /* For example: for(;;) { } */
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END ProcessorExpert */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.08]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
